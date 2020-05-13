b3e0c2e2522646da0e88b3038e222709355e91aa
AvidDabbler <walter.k.jenkins@gmail.com>
2020-05-13 08:47:47 -0500

    returns and saves list files for trips and vehicle updates

--- gtfsrt_logging.py
+++ gtfsrt_logging.py
@@ -12,8 +12,7 @@ dir = os.path.join(os.getcwd(), 'gtfs')
 # don't need this due to
 def saveTempData(data, filename):
     with open(filename, "w") as f:
-        jsondata = json.dumps(data, indent=2)
-        f.write(f'{jsondata}')
+        f.write(f'{data}')
         f.close()
 
     print(' ')
@@ -65,14 +64,11 @@ gtfsrt = {
     'vehicles': {
         'url': 'https://www.metrostlouis.org/RealTimeData/StlRealTimeVehicles.pb',
         'load': [],
-
     },
     'trips': {
         'url': 'https://www.metrostlouis.org/RealTimeData/StlRealTimeTrips.pb',
         'load': [],
-
-
-    }
+    },
 }
 
 
@@ -148,56 +144,45 @@ def getRealTime(gtfsrt):
                                 fields += f'{d}:"{value["vehicle"][data][d]}",'
                             else:
                                 fields += f'{d}:{value["vehicle"][data][d]},'
-            line = f"vehicles,{keys[:-1]} {fields[:-1]}"
+            line = f"vehicles,{keys[:-1]} {fields[:-1]} {timestamp}"
             print(line)
             pb['load'].append(line)
+        return pb['load']
 
-    def getTrips(pburl):
-        allTrips = {}
-        feed = parseDict(pburl)
+
+    def getTrips(pb):
+        feed = parseDict(pb['url'])
         for value in feed['entity']:
             print(value)
+            # {'id': '0', 'tripUpdate': {'trip': {'tripId': '2705906', 'startTime': '06:25:00', 'startDate': '20200513', 'routeId': '16370'}, 'stopTimeUpdate': [{'departure': {'time': '1589369100'}, 'stopId': '14403'}, {'departure': {'time': '1589369580'}, 'stopId': '4804'}, {'departure': {'time': '1589369640'}, 'stopId': '4806'}, {'departure': {'time': '1589369700'}, 'stopId': '4786'}, {'departure': {'time': '1589369760'}, 'stopId': '4810'}, {'departure': {'time': '1589369760'}, 'stopId': '4811'}, {'departure': {'time': '1589369820'}, 'stopId': '4814'}, {'departure': {'time': '1589369940'}, 'stopId': '4818'}, {'departure': {'time': '1589370060'}, 'stopId': '4822'}, {'departure': {'time': '1589370300'}, 'stopId': '13967'}, {'departure': {'time': '1589370540'}, 'stopId': '13526'}, {'departure': {'time': '1589370600'}, 'stopId': '15223'}, {'departure': {'time': '1589370660'}, 'stopId': '15623'}, {'departure': {'time': '1589370720'}, 'stopId': '15224'}, {'departure': {'time': '1589370780'}, 'stopId': '4826'}, {'departure': {'time': '1589370840'}, 'stopId': '4827'}, {'departure': {'time': '1589370840'}, 'stopId': '4828'}, {'departure': {'time': '1589370900'}, 'stopId': '15860'}, {'departure': {'time': '1589371020'}, 'stopId': '4829'}, {'departure': {'time': '1589371080'}, 'stopId': '14863'}, {'departure': {'time': '1589371080'}, 'stopId': '14862'}, {'departure': {'time': '1589371140'}, 'stopId': '14861'}, {'departure': {'time': '1589371200'}, 'stopId': '15732'}, {'departure': {'time': '1589371200'}, 'stopId': '4832'}, {'departure': {'time': '1589371440'}, 'stopId': '4834'}, {'departure': {'time': '1589371620'}, 'stopId': '15767'}, {'departure': {'time': '1589371620'}, 'stopId': '16065'}, {'departure': {'time': '1589371680'}, 'stopId': '15225'}, {'departure': {'time': '1589371860'}, 'stopId': '15012'}, {'departure': {'time': '1589371860'}, 'stopId': '15758'}, {'departure': {'time': '1589371920'}, 'stopId': '15733'}, {'departure': {'time': '1589371980'}, 'stopId': '15985'}, {'departure': {'time': '1589372040'}, 'stopId': '13528'}, {'departure': {'time': '1589372040'}, 'stopId': '587'}, {'departure': {'time': '1589372100'}, 'stopId': '13954'}, {'departure': {'time': '1589372160'}, 'stopId': '14828'}, {'departure': {'time': '1589372220'}, 'stopId': '14801'}, {'departure': {'time': '1589372340'}, 'stopId': '14133'}], 'vehicle': {'id': '4536', 'label': '65 - Outer Forty'}, 'timestamp': '1589371918'}}
+            keys = ''
+            fields = ''
+            timestamp = value['tripUpdate']['timestamp']
+            for data in value['tripUpdate']:
+                # print(value['tripUpdate'][data])
+                # print(type(value['tripUpdate'][data]))
+                if type(value["tripUpdate"][data]) == dict:
+                    for d in value["tripUpdate"][data]:
+                        if 'id' in d.lower():
+                            keys += f'{d}:{value["tripUpdate"][data][d]},'
+                        else:
+                            if type(value["tripUpdate"][data][d]) == str:
+                                fields += f'{d}:"{value["tripUpdate"][data][d]}",'
+                            else:
+                                fields += f'{d}:{value["tripUpdate"][data][d]},'
+            line = f"vehicles,{keys[:-1]} {fields[:-1]} {timestamp}"
+            print(line)
+            pb['load'].append(line)
+        return pb['load']
 
 
-    realtime_list = [
-        'https://www.metrostlouis.org/RealTimeData/StlRealTimeVehicles.pb',
-        'https://www.metrostlouis.org/RealTimeData/StlRealTimeTrips.pb'
-    ]
-
-    getVehicles(gtfsrt['vehicles'])
-    print(gtfsrt['vehicles']['load'])
-
-    # for item in realtime_list:
-    #     # if looking at vehicles
-    #     if item == 'https://www.metrostlouis.org/RealTimeData/StlRealTimeVehicles.pb':
-    #         print('writing vehicles...')
-    #         vehicles = getVehicles(item)
-    #         saveTempData(vehicles, r'leaflet\vehicles.json')
-    #         pass
-        # if looking at the trips file
-        # elif item == 'https://www.metrostlouis.org/RealTimeData/StlRealTimeTrips.pb':
-        #     print('writing trips...')
-        #     print(item)
-        #     trips = getTrips(item)
-        #     saveTempData(trips, r'leaflet\trips.json')
-        #     pass
-        # else:
-        #     print(item)
-        #     print('error')
-        #     return
-
-# getGTFS()
-
-# for file in gtfs:
-#     loadGTFS(file)
-
-# for file in gtfs:
-#     print(file['path'])
-#     print(file['load'])
+    saveTempData(getVehicles(gtfsrt['vehicles']), 'vehicles.txt')
+    saveTempData(getTrips(gtfsrt['trips']), 'trips.txt')
+    print(gtfsrt['trips']['load'])
 
 getRealTime(gtfsrt)
-#
-#
+
+
 # while 1==1:
 #   getRealTime()
 #   time.sleep(15)
